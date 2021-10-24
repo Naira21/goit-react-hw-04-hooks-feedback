281ac8ecf6cd2565e11f0ddb52a1c5afe2b85eaf
Naira Kazeian <nkazeian@gmail.com>
2021-10-24 22:45:07 +0300

    removed comments

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ export default function App() {
   const [bad, setBad] = useState(0);
 
   const counter = (type) => {
-    // const { type } = e.target;
     switch (type) {
       case "good":
         setGood((prevState) => prevState + 1);
@@ -62,69 +61,3 @@ export default function App() {
     </div>
   );
 }
-
-// import { Component } from "react";
-// import FeedbackOptions from "./components/FeedbackOptions";
-// import Notification from "./components/Notification";
-// import Statistics from "./components/Statistics";
-// import { Section } from "./components/Section";
-// import s from "./App.module.css";
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   counter = (type) => {
-//     this.setState((prevState) => ({
-//       [type]: prevState[type] + 1,
-//     }));
-//   };
-
-//   countTotalFeedback = () => {
-//     const { good, neutral, bad } = this.state;
-//     const result = good + neutral + bad;
-//     return result;
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     const result = this.countTotalFeedback();
-//     const percentage = (this.state.good * 100) / result;
-//     return Math.round(percentage);
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const { counter } = this;
-//     const total = this.countTotalFeedback();
-//     const positivePercentage = this.countPositiveFeedbackPercentage();
-//     return (
-//       <div className={s.wrapper}>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={["good", "neutral", "bad"]}
-//             onLeaveFeedback={counter}
-//           />
-//         </Section>
-
-//         <Section title="Statistics">
-//           {good === 0 && neutral === 0 && bad === 0 ? (
-//             <Notification message="No feedback given" />
-//           ) : (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={total}
-//               positivePercentage={positivePercentage}
-//             />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
